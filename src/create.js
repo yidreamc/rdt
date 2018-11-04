0ce539a6e3f9591d603f0104fd6d01e5dfa5dc63
yidreamc <yidreamc@gmail.com>
2018-11-04 21:26:28 +0800

    添加模板语法

--- src/create.js
+++ src/create.js
@@ -2,41 +2,45 @@
 const fs = require("fs");
 const path = require("path");
 const mkdirp = require('mkdirp');
-const getDirName = require('path').dirname;
 const config = require(path.join(__dirname, '../config.json'))
 
-function selectTemplate(ppath, tindex) {
+
+
+function selectTemplate(ppath, tindex, name) {
     const pa = fs.readdirSync(ppath);
     pa.forEach((ele, index) => {
         if (index === tindex) {
-            const templatePath = path.join(__dirname, '../template', ele)
-            create(templatePath);
+            const info = fs.statSync(path.join(ppath, ele));
+            if (info.isDirectory()) {
+                const templatePath = path.join(__dirname, '../template', ele);
+                create(templatePath, name);
+            }
+
         }
     });
 }
 
-function create(fpath) {
+function create(fpath, name) {
     const pa = fs.readdirSync(fpath);
     pa.forEach(ele => {
         const info = fs.statSync(path.join(fpath, ele))
         if (info.isDirectory()) {
-            create(path.join(fpath, ele));
+            create(path.join(fpath, ele), name);
         } else {
-            dealTemplate(path.join(fpath, ele));
+            dealTemplate(path.join(fpath, ele), name);
         }
     })
 }
 
-function dealTemplate(file) {
-    // console.log(file);
-    const name = file.match(/template\\\w+\\([\w\W]+)/)[1];
-    const des = path.join(config.path, name);
-    console.log(des);
-    console.log(getDirName(des));
-    mkdirp(getDirName(des));
+function dealTemplate(file, name) {
+    // 相对文件名字
+    const rename = file.match(/template\\\w+\\([\w\W]+)/)[1].replace(/\$name/g,name);
+    const des = path.join(config.path, rename);
+    const basename = path.basename(des);
+    mkdirp(path.dirname(des));
     fs.writeFileSync(des, fs.readFileSync(file));
-    
+
 }
 
 const templateRoot = path.join(__dirname, '../template')
-selectTemplate(templateRoot, 1);
+selectTemplate(templateRoot, 1, "Rate");
